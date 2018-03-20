b7e960584eb9044bf89ea0589a74ed0af40ac0bc
Fadeeva <fadeevaad1@gmail.com>
2018-03-20 16:49:04 +0300

    Finished footer and slider area

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// слайдер в футере
+
+    $(document).ready(function(){
+      $('.b-slide').slick({
+            slidesToShow: 4,
+            autoplay: false,
+            autoplaySpeed: 2000,
+            centerMode: false
+
+      });
+    });
