bae7e9ca2c5d2c433e07f590e42120e0bc3256e4
Fadeeva <fadeevaad1@gmail.com>
2018-03-27 12:29:52 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,23 @@
             slidesToShow: 4,
             autoplay: false,
             autoplaySpeed: 2000,
-            centerMode: false
-
+            centerMode: false,
+            prevArrow:'<a class="b-arrow b-arrow--footer-left" href="javascript:void(0)" title=""></a>',
+            nextArrow:'<a class="b-arrow b-arrow--footer-right" href="javascript:void(0)" title=""></a>'
       });
     });
+
+
+// слайдер в хедере
+
+
+    // $(document).ready(function(){
+    //   $('.b-slide-hrad').slick({
+    //         slidesToShow: 4,
+    //         autoplay: false,
+    //         autoplaySpeed: 2000,
+    //         centerMode: false,
+    //         prevArrow:'<a class="b-viewed__arrow right-header" href="javascript:void(0)" title=""></a>',
+    //         nextArrow:'<a class="b-viewed__arrow b-viewed__arrow--right" href="javascript:void(0)" title=""></a>'
+    //   });
+    // });
