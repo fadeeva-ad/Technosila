e1c44ceda9bb540bf30e6b6b521238f639201763
Fadeeva <fadeevaad1@gmail.com>
2018-04-02 13:39:36 +0300

    finished second slider and banner

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
 // слайдер в футере
 
-    $(document).ready(function(){
-      $('.b-slide').slick({
-            slidesToShow: 4,
-            autoplay: false,
-            autoplaySpeed: 2000,
-            centerMode: false,
-            prevArrow:'<a class="b-arrow b-arrow--footer-left" href="javascript:void(0)" title=""></a>',
-            nextArrow:'<a class="b-arrow b-arrow--footer-right" href="javascript:void(0)" title=""></a>'
-      });
-    });
+$(document).ready(function(){
+  $('.b-slide').slick({
+        slidesToShow: 4,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        centerMode: false,
+        prevArrow:'<a class="b-arrow b-arrow__tv-left" href="javascript:void(0)" title=""></a>',
+        nextArrow:'<a class="b-arrow b-arrow__tv-right" href="javascript:void(0)" title=""></a>'
+  });
+});
+
+$(document).ready(function(){
+  $('.b-products__wrapper').slick({
+        slidesToShow: 4,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        centerMode: false,
+        prevArrow:'<a class="b-arrow b-arrow__product-left" href="javascript:void(0)" title=""></a>',
+        nextArrow:'<a class="b-arrow b-arrow__product-right" href="javascript:void(0)" title=""></a>'
+  });
+});
+$(document).ready(function(){
+  $('.b-brands__wrapper').slick({
+        slidesToShow: 6,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        centerMode: false,
+        prevArrow:'<a class="b-arrow b-arrow__brand-right" href="javascript:void(0)" title=""></a>',
+        nextArrow:'<a class="b-arrow b-arrow__brand-left" href="javascript:void(0)" title=""></a>'
+  });
+});
+
 
 
 // слайдер в хедере
