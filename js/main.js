c419d74be8234bf4b30cf2690f833c8fc21d91a6
Fadeeva <fadeevaad1@gmail.com>
2018-04-20 16:21:08 +0300

    finished page, without tabs

--- js/main.js
+++ js/main.js
@@ -45,16 +45,5 @@ $(document).ready(function(){
 });
 
 
-// слайдер в хедере
 
 
-    // $(document).ready(function(){
-    //   $('.b-slide-hrad').slick({
-    //         slidesToShow: 4,
-    //         autoplay: false,
-    //         autoplaySpeed: 2000,
-    //         centerMode: false,
-    //         prevArrow:'<a class="b-viewed__arrow right-header" href="javascript:void(0)" title=""></a>',
-    //         nextArrow:'<a class="b-viewed__arrow b-viewed__arrow--right" href="javascript:void(0)" title=""></a>'
-    //   });
-    // });
